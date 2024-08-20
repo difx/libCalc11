1aec131475bf0e3ee09b18b038f9cc9062f00ab8
Phillips, Chris (S&A, Marsfield) <Chris.Phillips@csiro.au>
2024-08-20 20:21:30 +1000

    Set prefix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Check if an single argument
+if [ "$#" -eq 1 ]; then
+    prefix_path="$1"
+else
+    prefix_path="$(pwd)"
+fi
+
 autoreconf --force --install
-./configure
+./configure --prefix="$prefix_path"
+
