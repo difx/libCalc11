f706085f46cd4f29c7548df7c6fb19823fdae40d
Chris Phillips <Chris.Phillips@csiro.au>
2024-10-14 16:39:22 +1100

    Just load single source

--- src/load_source.f
+++ src/load_source.f
@@ -1,4 +1,4 @@
-      integer*4 function LOAD_SOURCE( NSRC, SRC_NAME, SRC_RA, SRC_DEC)
+      integer*4 function LOAD_SOURCE(SRC_NAME, SRC_RA, SRC_DEC)
       implicit none
 
       INCLUDE 'cmxsr11.i'
@@ -10,15 +10,15 @@
       Character*20 SrcName(MAX_ARC_SRC)
       Equivalence (LNSTAR(1,1), SrcName(1))
 
-      IF ( NSRC .GT. MAX_ARC_SRC ) THEN
-         LOAD_SOURCE = -1
-      ELSE
-        SrcName(NSRC) = SRC_NAME
-        RADEC(1,NSRC) = SRC_RA
-        RADEC(2,NSRC) = SRC_DEC
-        LOAD_SOURCE = 0
-      ENDIF
+      NSRC=1
+      
+!     IF ( NSRC .GT. MAX_ARC_SRC ) THEN
+!       LOAD_SOURCE = -1
+!     ELSE
+      SrcName(NSRC) = SRC_NAME
+      RADEC(1,NSRC) = SRC_RA
+      RADEC(2,NSRC) = SRC_DEC
+      LOAD_SOURCE = 0
 
       RETURN
-
       END
